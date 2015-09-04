f4a641197eb91732a2dab1ece927fa040acbe58b
Sandeep <srisandeep@hotmail.com>
2015-09-05 00:18:47 +0800

    Continue/Quit added.

--- capitalCities.py
+++ capitalCities.py
@@ -1,6 +1,7 @@
 import urllib
 from bs4 import BeautifulSoup
 
+
 # website is parsed and a dictionary is returned. Key = country and Value = capital.
 def capitalOf():
     sourceCode = urllib.urlopen("http://www.manythings.org/vocabulary/lists/2/words.php?f=countries_and_capitals").read()
@@ -13,16 +14,17 @@ def capitalOf():
     return countryCapitalMapper
 
 # User is asked for input and if it is a known country, capital is printed. Otherwise, capital is unknown.
-def whatIsTheCapital():
-    country = raw_input("Key in the country: ").capitalize()
-    countryCapitalMapper = capitalOf()
-    if country in capitalOf():
-        capital = countryCapitalMapper[country]
-        print "The capital is " + capital + "."
-    else:
-        print "Unknown."
-
-whatIsTheCapital()
+if __name__ == "__main__":
+    while True:
+        country = raw_input("Key in the country or quit to exit: ").capitalize()
+        if country == "quit":
+            break
+        countryCapital = capitalOf()
+        if country in capitalOf():
+            capital = countryCapital[country]
+            print "The capital is " + capital + "."
+        else:
+            print "Unknown."
 
 
 
